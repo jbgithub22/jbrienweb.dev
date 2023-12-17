167ab18207258f2560250c3ffeb9acd9cd9a2d30
jb.github22 <jb.github22@gmail.com>
2023-12-17 18:19:23 +0800

    Chaned js 'switch' statements to object literals.

--- apps/drumKit/index.js
+++ apps/drumKit/index.js
@@ -1,45 +1,58 @@
+// function handleClick(key) {
+//     switch (key) {
+//         case "w":
+//             var audio = new Audio("sounds/tom-1.mp3");
+//             audio.play();
+//             break;
 
+//         case "a":
+//             var audio = new Audio("sounds/tom-2.mp3");
+//             audio.play();
+//             break;
+
+//         case "s":
+//             var audio = new Audio("sounds/tom-3.mp3");
+//             audio.play();
+//             break;
+
+//         case "d":
+//             var audio = new Audio("sounds/tom-4.mp3");
+//             audio.play();
+//             break;
+
+//         case "j":
+//             var audio = new Audio("sounds/snare.mp3");
+//             audio.play();
+//             break;
+
+//         case "k":
+//             var audio = new Audio("sounds/crash.mp3");
+//             audio.play();
+//             break;
+
+//         case "l":
+//             var audio = new Audio("sounds/kick-bass.mp3");
+//             audio.play();
+//             break;
+
+//         default:
+//             console.log()
+//             break;
+//     }
+// }
 
 function handleClick(key) {
-    switch (key) {
-        case "w":
-            var audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-            break;
-
-        case "a":
-            var audio = new Audio("sounds/tom-2.mp3");
-            audio.play();
-            break;
-
-        case "s":
-            var audio = new Audio("sounds/tom-3.mp3");
-            audio.play();
-            break;
-
-        case "d":
-            var audio = new Audio("sounds/tom-4.mp3");
-            audio.play();
-            break;
-
-        case "j":
-            var audio = new Audio("sounds/snare.mp3");
-            audio.play();
-            break;
-
-        case "k":
-            var audio = new Audio("sounds/crash.mp3");
-            audio.play();
-            break;
-
-        case "l":
-            var audio = new Audio("sounds/kick-bass.mp3");
-            audio.play();
-            break;
-
-        default:
-            console.log()
-            break;
+    let audio = {
+        "w" : new Audio("sounds/tom-1.mp3"),
+        "a" : new Audio("sounds/tom-2.mp3"),
+        "s" : new Audio("sounds/tom-3.mp3"),
+        "d" : new Audio("sounds/tom-4.mp3"),
+        "j" : new Audio("sounds/snare.mp3"),
+        "k" : new Audio("sounds/crash.mp3"),
+        "l" : new Audio("sounds/kick-bass.mp3")
+    }
+    if (audio.hasOwnProperty(key)) {
+        audio[key].play();
     }
 }
 
